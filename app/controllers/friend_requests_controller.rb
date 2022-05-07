6cc3a893c981a2dd51cb611e7b3966a6a03a6c6e
Bane-999 <bane-93erep@hotmail.com>
2022-05-08 00:44:18 +0200

    add validations

---
+++ app/controllers/friend_requests_controller.rb
@@ -0,0 +1,48 @@
+class FriendRequestsController < ApplicationController
+  before_action :set_friend_request, only: %i[ show edit update destroy ]
+
+  
+  # POST /friend_requests or /friend_requests.json
+  def create
+    
+    if current_user.id.to_s != friend_request_params[:request_id]
+      @friend_request = current_user.friend_requests.new(friend_request_params) 
+    else
+      @friend_request = current_user.friend_requests.new(user_id: 13)
+    end
+
+    respond_to do |format|
+      if @friend_request.save
+        99.times do
+          puts @friend_request.save
+        end
+        format.html { redirect_to users_path, notice: "Friend request was successfully created." }        
+      else
+        format.html { redirect_to users_path, notice: "Friend request was NOT successfully created!" }
+      end
+    end
+
+  end  
+
+  # DELETE /friend_requests/1 or /friend_requests/1.json
+  # def destroy
+  #   @friend_request.destroy
+
+  #   respond_to do |format|
+  #     format.html { redirect_to friend_requests_url, notice: "Friend request was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friend_request
+      @friend_request = FriendRequest.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def friend_request_params
+      # params.fetch(:friend_request, {})
+      params.require(:friend_request).permit(:request_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index]
   devise_for :users, controllers: {registrations: "registrations"}
   resources :posts
+  resources :friend_requests, only: [:create]
   
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
